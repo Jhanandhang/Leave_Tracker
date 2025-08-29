a9de02b6d5b25dd65919b09094f1e950c9a7813d
Jhanandhang <jhanandhan.g@gmail.com>
2025-08-29 09:46:10 +0530

    First commit1

---
+++ src/main/java/com/project/hrleave/HrleaveApplication.java
@@ -0,0 +1,13 @@
+package com.project.hrleave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrleaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrleaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/hrleave/HrleaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.hrleave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrleaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
